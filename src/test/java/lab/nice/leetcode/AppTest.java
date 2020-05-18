64db1802eefeeeefdbbd7117c0d0382cbbffc63b
nicetoseeyou <hello4world@yeah.net>
2020-05-18 22:39:04 +0800

    test no.1 add two sum

--- src/test/java/lab/nice/leetcode/AppTest.java
+++ src/test/java/lab/nice/leetcode/AppTest.java
@@ -1,18 +1,19 @@
 package lab.nice.leetcode;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testTwoSum() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] expect = {0, 1};
+        TwoSum twoSum = new TwoSum();
+        int[] result = twoSum.twoSum(nums, target);
+        Assert.assertArrayEquals("Expected result is [0, 1]", expect, result);
     }
 }
